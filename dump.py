b8c832a6c2ca4336f5cab1d9e2fb7434dadbb363
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-09-21 15:01:56 +0700

    Change http to https

--- dump.py
+++ dump.py
@@ -2,13 +2,13 @@
 
 from lib.dumper import Dumper
 
-f0nt_base ="http://www.f0nt.com/release/"
+f0nt_base = "https://www.f0nt.com/release/"
 
 if __name__ == "__main__":
     dumper = Dumper(f0nt_base)
     count = dumper.get_page_count()
     for page in range(1, count):
-       fonts = dumper.get_page_fonts(page)
-       for font in fonts:
-           dumper.get_font(font)
+        fonts = dumper.get_page_fonts(page)
+        for font in fonts:
+            dumper.get_font(font)
     dumper.stamp()
