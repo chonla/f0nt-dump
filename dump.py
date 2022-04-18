d4c7e2da83b4240578168bd7313854c0b2e93afe
Chonlasith Jucksriporn <chonlasith@gmail.com>
2022-04-18 20:26:44 +0700

    feat: Make python3 compatibility

--- dump.py
+++ dump.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from lib.dumper import Dumper
 
-f0nt_base = "https://www.f0nt.com/release/"
+f0nt_base = "https://www.f0nt.com/release"
 
 if __name__ == "__main__":
+    print("Backing up fonts ...")
     dumper = Dumper(f0nt_base)
     count = dumper.get_page_count()
+    print("> {} page(s) found ...".format(count))
     for page in range(1, count):
         fonts = dumper.get_page_fonts(page)
+        print("> {} font(s) found ...".format(len(fonts)))
         for font in fonts:
             dumper.get_font(font)
     dumper.stamp()
--- lib/dumper.py
+++ lib/dumper.py
@@ -1,7 +1,7 @@
 import requests
 import re
 import os
-import urlparse
+from urllib.parse import urlparse, urlsplit
 import zipfile
 import shutil
 from datetime import datetime
@@ -10,32 +10,35 @@ from datetime import datetime
 class Dumper():
     def __init__(self, base):
         self.base = base
+        self.default_headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+            "Referer": "https://www.f0nt.com/release"
+        }
 
     def get_page_count(self):
-        url = self.base
-        r = requests.head(url, allow_redirects=True)
-        content = requests.get(r.url)
-        result = re.search(
-            "<a class=\"last\" href=\"https:\/\/www\.f0nt\.com\/release\/page\/(\d+)\/\">", content.text)
+        print("Retrieving page count ...")
+        url = "{}/".format(self.base)
+        headers = self.default_headers
+        content = requests.get(url, allow_redirects=True, headers = headers)
+        result = re.search("<a class=\"last\" aria-label=\"Last Page\" href=\"https:\/\/www\.f0nt\.com\/release\/page\/(\d+)\/\">", content.text)
         if result:
             return int(result.group(1))
-        else:
-            return 0
+        return 0
 
     def get_page_fonts(self, page_no):
-        print "Open page " + str(page_no)
-        url = self.base + 'page/' + str(page_no)
-        r = requests.head(url, allow_redirects=True)
-        content = requests.get(r.url)
+        print("Accessing page {} ...".format(str(page_no)))
+        headers = self.default_headers
+        url = "{}/page/{}".format(self.base, str(page_no))
+        content = requests.get(url, allow_redirects=True, headers = headers)
         result = re.findall(
             "<a href=\"https:\/\/www\.f0nt\.com\/release\/([^\/]+)\/\" rel=\"bookmark\">", content.text)
         return result
 
     def get_font(self, font_name):
-        print "Get font " + font_name
-        url = self.base + font_name + '/'
-        r = requests.head(url, allow_redirects=True)
-        content = requests.get(r.url)
+        print("Getting font {} ...".format(font_name))
+        headers = self.default_headers
+        url = "{}/{}/".format(self.base, font_name)
+        content = requests.get(url, allow_redirects=True, headers = headers)
         download_pos = content.text.find("<div id=\"download\">")
         if download_pos >= 0:
             trimmed_content = content.text[download_pos +
@@ -46,48 +49,43 @@ class Dumper():
                 font_url = result.group(1)
                 self.download_and_unpack(font_url, font_name)
         else:
-            self.err_log("Font " + font_name +
-                         " cannot be download. May be it contains external link. Check " + url + " for detail.")
+            self.err_log("> Font {} cannot be download. May be it contains external link. Check {} for detail.".format(font_name, url))
 
     def err_log(self, txt):
-        print txt
+        print(txt)
         stamp = datetime.now()
         text_file = open("error_log.txt", "a+")
-        text_file.write(stamp.strftime(
-            "%Y-%m-%d %H:%M:%S") + " - " + txt + "\n")
+        text_file.write("{} - {}\n".format(stamp.strftime("%Y-%m-%d %H:%M:%S"), txt))
         text_file.close()
 
     def stamp(self):
         stamp = datetime.now()
         text_file = open("dump_stamp.txt", "w")
-        text_file.write("Last dump: " + stamp.strftime("%Y-%m-%d %H:%M:%S"))
+        text_file.write("Last dump: {}".format(stamp.strftime("%Y-%m-%d %H:%M:%S")))
         text_file.close()
 
     def download_and_unpack(self, url, font_name):
-        print "Downloading " + url + " ..."
-        file_name = os.path.basename(urlparse.urlsplit(url).path)
-        headers = {"Referer": "https://www.f0nt.com/release/" + font_name}
+        print("> Downloading {} ...".format(url))
+        file_name = os.path.basename(urlsplit(url).path)
+        headers = self.default_headers | {"Referer": "https://www.f0nt.com/release/" + font_name}
         content = requests.get(url, stream=True, headers=headers)
-        if not os.path.exists('tmp'):
-            os.makedirs('tmp')
+        os.makedirs('tmp', exist_ok = True)
+        file_list = []
         with open('tmp/' + file_name, 'wb') as f:
             for chunk in content.iter_content(chunk_size=1024):
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
 
         if content.headers['Content-Type'] == "application/zip":
-            if not os.path.exists('tmp/t'):
-                os.makedirs('tmp/t')
-            print "Ripping fonts from " + file_name + " ..."
+            os.makedirs('tmp/t', exist_ok = True)
+            print("Ripping fonts from " + file_name + " ...")
             try:
                 zip = zipfile.ZipFile('tmp/' + file_name)
                 file_list = zip.namelist()
-                file_list = filter(lambda x: (
-                    x[-4:].lower() == ".ttf") and (x[:1] != ".") and (x.find("/.") == -1), file_list)
+                file_list = list(filter(lambda x: (x[-4:].lower() == ".ttf") and (x[:1] != ".") and ("/." not in x), file_list))
                 zip.extractall('tmp/t/', file_list)
-            except RuntimeError:
-                if not os.path.exists('zip'):
-                    os.makedirs('zip')
+            except (ValueError, RuntimeError):
+                os.makedirs('zip', exist_ok = True)
                 shutil.copyfile('tmp/' + file_name, 'zip/' + file_name)
                 self.err_log("Unable to extract " + file_name +
                              ". See zip/" + file_name + " for detail.")
@@ -95,10 +93,10 @@ class Dumper():
                 shutil.rmtree('tmp/')
                 return
 
-            if not os.path.exists('ttf'):
-                os.makedirs('ttf')
+            os.makedirs('ttf', exist_ok = True)
             for ttf in file_list:
                 ttf_name = os.path.basename(ttf)
+                print("> Copying tmp/t/{} to ttf/{}".format(ttf, ttf_name))
                 shutil.copyfile('tmp/t/' + ttf, 'ttf/' + ttf_name)
         else:
             self.err_log("Font " + font_name +
